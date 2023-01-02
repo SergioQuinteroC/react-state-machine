bf2ef1aee29d8428d81d61a0790519932bb782ce
SergioQuinteroC <sergio.quinteroc@udea.edu.co>
2023-01-02 18:31:27 -0500

    feat transicions and events

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './Nav.css';
+import React from "react";
+import "./Nav.css";
 
 export const Nav = ({ state, send }) => {
-
+  const goToWelcome = () => {
+    send("CANCEL");
+  };
   return (
-    <nav className='Nav'>
-      <h1 className='Nav-logo'>Book a fly ✈</h1>
-        <button className='Nav-cancel button-secondary'>Cancelar</button>
+    <nav className="Nav">
+      <h1 className="Nav-logo">Book a fly ✈</h1>
+      {!state.matches("initial") && (
+        <button className="Nav-cancel button-secondary" onClick={goToWelcome}>
+          Cancelar
+        </button>
+      )}
     </nav>
   );
-}; 
+};
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,23 +1,44 @@
-import React, { useState } from 'react';
-import './Search.css';
+import React, { useState } from "react";
+import "./Search.css";
 
-export const Search = ({ send, context }) => {
-  const [flight, setFlight] = useState('');
+export const Search = ({ send }) => {
+  const [flight, setFlight] = useState("");
+
+  const goToPassangers = () => {
+    send("CONTINUE");
+  };
 
   const handleSelectChange = (event) => {
     setFlight(event.target.value);
   };
 
-  const options = ['Mexico', 'Venezuela', 'Colombia'];
+  const options = ["Mexico", "Venezuela", "Colombia"];
 
   return (
-    <div className='Search'>
-      <p className='Search-title title'>Busca tu destino</p>
-      <select id="country" className='Search-select' value={flight} onChange={handleSelectChange}>
-        <option value="" disabled defaultValue>Escoge un país</option>
-        {options.map((option) => <option value={option} key={option}>{option}</option>)}
+    <div className="Search">
+      <p className="Search-title title">Busca tu destino</p>
+      <select
+        id="country"
+        className="Search-select"
+        value={flight}
+        onChange={handleSelectChange}
+      >
+        <option value="" disabled defaultValue>
+          Escoge un país
+        </option>
+        {options.map((option) => (
+          <option value={option} key={option}>
+            {option}
+          </option>
+        ))}
       </select>
-      <button disabled={flight === ''} className='Search-continue button'>Continuar</button>
+      <button
+        onClick={goToPassangers}
+        disabled={flight === ""}
+        className="Search-continue button"
+      >
+        Continuar
+      </button>
     </div>
   );
-}; 
+};
